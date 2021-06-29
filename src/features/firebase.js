fdee486e83a6b3f7bffc012885dc306243370595
Milan Zadfiya <41838197+milan2798@users.noreply.github.com>
2021-06-29 22:46:01 +0530

    Update firebase config

--- src/features/firebase.js
+++ src/features/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD--dDkyfZV2uDoPY4hwJ8Lv03grfeSwl4",
-    authDomain: "snapchat-clone-2104.firebaseapp.com",
-    projectId: "snapchat-clone-2104",
-    storageBucket: "snapchat-clone-2104.appspot.com",
-    messagingSenderId: "138941733699",
-    appId: "1:138941733699:web:8875f2ffd85809faf66df6",
-    measurementId: "G-BSEYPF4WH6"
+    apiKey: "AIzaSyD--dDkyfZV2uDoJ8Lv03grfeSwl4",
+    authDomain: "snapchat.firebaseapp.com",
+    projectId: "snapchat",
+    storageBucket: "snapchatmmm.appspot.com",
+    messagingSenderId: "131733699",
+    appId: "1:138941733699:web:8875f85809faf66df6",
+    measurementId: "G-BSE4WH6"
   };
 const firebaseApp=firebase.initializeApp(firebaseConfig);
 const db=firebaseApp.firestore();
